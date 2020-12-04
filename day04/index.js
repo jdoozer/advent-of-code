12cf7bce3ec03ad5d57cef0ce80c6db84f03da02
Joanna Cohen <joanna.cohen@gmail.com>
2020-12-04 00:07:03 -0800

    day 4 working now

--- day04/index.js
+++ day04/index.js
@@ -27,15 +27,10 @@ const passportCountByCheck = (validCheckFn) => passportList.reduce(
 );
 
 const validPassportCount = passportCountByCheck(isPassportValid);
-// const validPassportCount = passportList.reduce(
-//     (validCount, passport) => validCount + (isPassportValid(passport) ? 1 : 0),
-//     0
-// );
 
 console.log(validPassportCount);
 
 // part 2
-
 // byr (Birth Year) - four digits; at least 1920 and at most 2002.
 // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
 // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
@@ -48,13 +43,13 @@ console.log(validPassportCount);
 // cid (Country ID) - ignored, missing or not.
 
 const fieldValidations = [
-    /byr:(19[2-9][0-9]|200[0-2])/m,
-    /iyr:(20(1[0-9]|20))/m,
-    /eyr:(20(2[0-9]|30))/m,
-    /hgt:(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in)/m,
-    /hcl:#([0-9a-f]){6}/m,
-    /ecl:(amb|blu|brn|gry|grn|hzl|oth)/m,
-    /pid:([0-9]){9}/m,
+    /byr:(19[2-9][0-9]|200[0-2])\b/m,
+    /iyr:(20(1[0-9]|20))\b/m,
+    /eyr:(20(2[0-9]|30))\b/m,
+    /hgt:(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in)\b/m,
+    /hcl:#([0-9a-f]){6}\b/m,
+    /ecl:(amb|blu|brn|gry|grn|hzl|oth)\b/m,
+    /pid:([0-9]){9}\b/m,
 ];
 
 const isPassportValidStrict = (passport) => fieldValidations.every(regex => regex.test(passport));
